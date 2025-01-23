f511c0a860cd6790214e00fbbda5ec58c9d3d0ad
azahin1 <z4hin.4brar@gmail.com>
2025-01-22 22:50:34 -0700

    add spinning on throw

--- GameFiles/Scripts/Enemy.gd
+++ GameFiles/Scripts/Enemy.gd
@@ -2,3 +2,6 @@ class_name Enemy
 extends CharacterBody2D
 
 @export var maxSpeed: float = 500.0
+
+func onDeath() -> void:
+	queue_free()
--- GameFiles/Scripts/PlayerLogic.gd
+++ GameFiles/Scripts/PlayerLogic.gd
@@ -4,13 +4,17 @@ extends RigidBody2D
 #-- Conections
 @export var host: Enemy
 @onready var animator: AnimationPlayer = $Animator
+@onready var sprite: Sprite2D = $WeaponSprite
 
 #-- Variables
-@export var throwPower: float = 1000.0
+@export var throwPower: float = 1100.0
 var forwardDirection: Vector2 = Vector2(1, 0)
 var forwardRadians: float = 0.0
 @export var rotationOffset: float = 0.0 # used for animations
 @export var isSwinging: bool = false
+
+func _ready():
+	swapNewHost(host)
 
 func _process(delta) -> void:
 	if host:
@@ -25,6 +29,8 @@ func _physics_process(delta) -> void:
 		moveHost(delta)
 	else: # slow down when thrown
 		linear_velocity = linear_velocity.move_toward(Vector2.ZERO, throwPower*delta)
+		animator.speed_scale = linear_velocity.abs().length()/throwPower
+		print(animator.speed_scale)
 
 func faceInDirection() -> void: # faces the sword in the direction of the mouse or right stick
 	var newDirection: Vector2 = (get_global_mouse_position() - position).normalized()
@@ -42,9 +48,19 @@ func moveHost(delta: float) -> void: # moves the host instead of the player node
 
 func throwSelf() -> void: # throws self in the current looking direction
 	if Input.is_action_just_pressed("throw"):
+		animator.play("ThrowStartup")
+
+func _on_animator_animation_finished(anim_name) -> void: # helper function for throwing
+	if anim_name == "ThrowStartup":
+		host.onDeath()
 		host = null # removes self from host
 		linear_velocity = forwardDirection*throwPower
-		
+		animator.play("Speen")
+
 func swing() -> void: # playes the swing sword animation when the attack button is pressed
 	if Input.is_action_just_pressed("attack"):
 		animator.play("WeaponSwing")
+
+func swapNewHost(newHost: Enemy) -> void:
+	host = newHost
+	sprite.position.x = 50
