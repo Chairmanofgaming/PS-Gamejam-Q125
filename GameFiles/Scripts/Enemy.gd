403697e2731cc81c9566c93503aca8a190ad94a1
azahin1 <z4hin.4brar@gmail.com>
2025-01-22 17:13:23 -0700

    add wasd movement

--- GameFiles/Scripts/Enemy.gd
+++ GameFiles/Scripts/Enemy.gd
@@ -1,3 +1,4 @@
 class_name Enemy
 extends CharacterBody2D
 
+@export var maxSpeed: int = 300
--- GameFiles/Scripts/PlayerLogic.gd
+++ GameFiles/Scripts/PlayerLogic.gd
@@ -4,10 +4,23 @@ extends Node2D
 @export var host: Enemy
 
 func _process(delta) -> void:
-	faceInDirection()
+	if host:
+		faceInDirection()
+		followHost()
+	
+func _physics_process(delta) -> void:
+	if host:
+		moveHost(delta)
 
-func faceInDirection():
+func faceInDirection() -> void:
 	var newDirection: Vector2 = (get_global_mouse_position() - position).normalized()
 	if newDirection != Vector2.ZERO:
 		rotation = atan2(newDirection.y, newDirection.x)
 
+func moveHost(delta: float) -> void:
+	var moveDirection: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	host.velocity = moveDirection * host.maxSpeed
+	host.move_and_slide()
+	
+func followHost() -> void:
+	position = host.position
