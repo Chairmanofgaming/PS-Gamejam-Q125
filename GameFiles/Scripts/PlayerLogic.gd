2a6784e7e45034bde6bc5e6a5013ef7c27bdb23f
azahin1 <z4hin.4brar@gmail.com>
2025-01-23 00:14:57 -0700

    bug fix when swapping hosts

--- GameFiles/Scripts/PlayerLogic.gd
+++ GameFiles/Scripts/PlayerLogic.gd
@@ -6,6 +6,7 @@ extends Area2D
 @onready var animator: AnimationPlayer = $Animator
 @onready var sprite: Sprite2D = $WeaponSprite
 @onready var speenCollider: CollisionShape2D = $SpeenCollider
+@onready var swingCollider: CollisionShape2D = $SwingingCollider
 
 #-- Variables
 @export var throwPower: float = 1100.0
@@ -14,21 +15,24 @@ var forwardRadians: float = 0.0
 @export var rotationOffset: float = 0.0 # used for animations
 @export var isSwinging: bool = false
 var velocity: Vector2 = Vector2.ZERO
+var hitList: Array[Enemy] = []
 
 func _ready():
 	swapNewHost(host)
 
 func _process(delta) -> void:
+	swingCollider.disabled = !isSwinging
 	if host:
 		position = host.position # follows the host
 		rotation = forwardRadians + rotationOffset
-		faceInDirection()
-		throwSelf()
-		swing()
 
 func _physics_process(delta) -> void:
 	if host:
+		speenCollider.disabled = true
 		moveHost(delta)
+		swing()
+		throwSelf()
+		faceInDirection()
 	else: # slow down when thrown
 		velocity = velocity.move_toward(Vector2.ZERO, throwPower*delta)
 		animator.speed_scale = velocity.abs().length()/throwPower
@@ -46,7 +50,7 @@ func moveHost(delta: float) -> void: # moves the host instead of the player node
 		host.velocity = host.velocity.move_toward(moveDirection*host.maxSpeed, host.maxSpeed*delta*4)
 	else: # decelerate at a faster rate
 		host.velocity = host.velocity.move_toward(Vector2.ZERO, host.maxSpeed*delta*8)
-	host.move_and_collide(host.velocity * delta)
+	host.move_and_slide()
 
 func throwSelf() -> void: # throws self in the current looking direction
 	if Input.is_action_just_pressed("throw"):
@@ -59,20 +63,22 @@ func _on_animator_animation_finished(anim_name) -> void: # helper function for t
 		speenCollider.disabled = false
 		velocity = forwardDirection*throwPower
 		animator.play("Speen")
+	if anim_name == "WeaponSwing":
+		hitList = []
 
 func swing() -> void: # playes the swing sword animation when the attack button is pressed
 	if Input.is_action_just_pressed("attack"):
 		animator.play("WeaponSwing")
 
 func swapNewHost(newHost: Enemy) -> void: # swaps to new host
-	animator.stop()
 	animator.speed_scale = 1
-	host = newHost
+	animator.stop()
 	sprite.position.x = 50
-	speenCollider.disabled = true
+	host = newHost
 
 func _on_body_entered(body: Enemy): # collision detection
-	if host: # you can only hit with a swing if you have a host
+	if host and body not in hitList: # you can only hit with a swing if you have a host
 		print("hit")
-	else:
-		call_deferred("swapNewHost", body)
+		hitList.append(body)
+	elif not host: # only can swap host when you don't have a host currently
+		swapNewHost(body)
